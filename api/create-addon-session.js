76fe5764450f3b04fc085b24a3187f7840467835
ComeAliveStudio <office@comealive.vision>
2026-04-04 19:51:04 +0200

    Update create-addon-session.js

--- api/create-addon-session.js
+++ api/create-addon-session.js
@@ -1,1 +1,28 @@
+import Stripe from 'stripe'
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: [
+        {
+          price: 'price_YOUR_7_EURO_ID',
+          quantity: 1
+        }
+      ],
+      success_url: `${process.env.APP_URL}?addon=success`,
+      cancel_url: `${process.env.APP_URL}?addon=cancel`
+    })
+
+    return res.status(200).json({ url: session.url })
+  } catch (err) {
+    return res.status(500).json({ error: err.message })
+  }
+}
